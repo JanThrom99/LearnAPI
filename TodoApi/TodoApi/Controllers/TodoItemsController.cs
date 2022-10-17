4756a3698050cc857944e130a0cc3b10c3fcb262
Jan Throm <Jan.Throm@TecAlliance.net>
2022-10-17 14:07:43 +0200

    created comments for the todoAPI

--- TodoApi/TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/TodoApi/Controllers/TodoItemsController.cs
@@ -4,7 +4,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
@@ -14,8 +14,13 @@ namespace TodoApi.Controllers
         {
             _context = context;
         }
-        #region API stuff
-        // GET: api/TodoItems
+        #region API Methods
+        /// <summary>
+        /// Method for getting all the stored items
+        /// </summary>
+        /// <returns>
+        /// - A Collection of all items
+        /// </returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
@@ -23,7 +28,13 @@ namespace TodoApi.Controllers
                 .Select(x => ItemToDTO(x))
                 .ToListAsync();
         }
-        // GET: api/TodoItems/5
+        /// <summary>
+        /// Method for getting a specific item by ID.
+        /// </summary>
+        /// <param name="id"> The ID the user gives into the method.</param>
+        /// <returns>
+        /// - A TodoItemDTO object and a 201 Response.
+        /// </returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
@@ -36,7 +47,16 @@ namespace TodoApi.Controllers
 
             return ItemToDTO(todoItem);
         }
-        // PUT: api/TodoItems/5
+        /// <summary>
+        /// Method which is used to update an existing item.
+        /// </summary>
+        /// <param name="id">The ID of the item the user wants to update.</param>
+        /// <param name="todoItemDTO">The "new Info" the user wants to store in the existing item.</param>
+        /// <returns>
+        /// - A "BadRequest - 400 Response" if the ID isnt matchable
+        /// - A "NotFound - 404 Response" if the existing item with the correct ID is null or if the update is complete and the save didnt work properly
+        /// - A "NoContent - 204 Response" if the update worked and the old item now has the new values
+        /// </returns> 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateTodoItem(long id, TodoItemDTO todoItemDTO)
         {
@@ -65,7 +85,13 @@ namespace TodoApi.Controllers
 
             return NoContent();
         }
-        // POST: api/TodoItems
+        /// <summary>
+        /// Method for creating a new Item.
+        /// </summary>
+        /// <param name="todoItemDTO">The Item the user wants to create with its specific values.</param>
+        /// <returns>
+        /// - A TodoItemDTO object and a 201 response.
+        /// </returns>
         [HttpPost]
         public async Task<ActionResult<TodoItemDTO>> CreateTodoItem(TodoItemDTO todoItemDTO)
         {
@@ -83,7 +109,14 @@ namespace TodoApi.Controllers
                 new { id = todoItem.Id },
                 ItemToDTO(todoItem));
         }
-        // DELETE: api/TodoItems/5
+        /// <summary>
+        /// Method to delete the desired Item. 
+        /// </summary>
+        /// <param name="id">The ID of the item the user wants to delete</param>
+        /// <returns>
+        /// - A "NoContent - 204 Response" if the item has successfully been deleted.
+        /// - A "NotFound - 404 Response" if the to be deleted item is null
+        /// </returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTodoItem(long id)
         {
@@ -102,11 +135,22 @@ namespace TodoApi.Controllers
         #endregion
 
         #region other Methods
+        /// <summary>
+        /// Method which returns a bool that tells the user whether there is an item with the ID he gave into the method.
+        /// </summary>
+        /// <param name="id">The ID of the todoItem the user wants to receive/check.</param>
+        /// <returns>
+        /// - A bool
+        /// </returns>
         private bool TodoItemExists(long id)
         {
             return _context.TodoItems.Any(e => e.Id == id);
         }
-
+        /// <summary>
+        ///  //TODO comment 
+        /// </summary>
+        /// <param name="todoItem"></param>
+        /// <returns></returns>
         private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
             new TodoItemDTO
             {
